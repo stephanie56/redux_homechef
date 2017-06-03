b74d9214def2346a16fcb7ec7e591dfba517b159
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-06-03 15:45:32 -0400

    Configure redux devtool

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 import recipes from './reducers/reducer_recipes';
 
-const store = createStore(recipes);
+const store = createStore(
+  recipes,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
