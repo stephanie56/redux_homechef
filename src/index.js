f1907d225ee89a73bf8bb22f0784aa852c5b0c99
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-06-11 17:42:20 -0400

    Added redux-logger and redux-persist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 // React
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
+
+
+// Store
+import store from './store';
 
 // Component
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default class AppProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { rehydrated: false }
+  }
+
+  componentWillMount(){
+    persistStore(store, {}, () => {
+      this.setState({ rehydrated: true });
+    })
+  }
+  render() {
+    if(!this.state.rehydrated){
+      return <div>Loading...</div>
+    }
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+  }
+}
+
+ReactDOM.render(<AppProvider />, document.getElementById('root'));
